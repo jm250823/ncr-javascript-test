f5a4890d94727616c312dae03e670f728a7684bc
Martin <jm250823@ncr.com>
2020-10-09 14:17:39 -0300

    ejercicio original

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # web-platform-m4q7c4
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-m4q7c4)
+
+1. Consumir servicio especificado en `script.js` como `API_ENDPOINT` (recomendacion: usar Fetch).
+2. Construir la clase `Account` con sus metodos.
+3. Transformar el resultado de la consulta a un array de Accounts filtrando las cuentas que no tengan un tipo valido.
+4. Mostrar en HTML la informacion de las cuentas hasta un maximo de 5 resultados.
+5. Agregar un boton que al clickear permita cambiar por los siguientes 5 resultados.
--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('hello!')
+class AccType{
+    //id: number
+    //codigo: string
+    //nombre: string
+  }
+  
+  class Account{
+    //tipo: AccType
+    //nombre: string
+    //saldo: number
+  
+    toHtml(){
+      return `
+        <div>Cuenta de ejemplo</div>
+      `
+    }
+  }
+  
+  async function fetchAPI(url){
+    return []
+  }
+  
+  function mapToAccounts(resp){
+    //Mapear respuesta a un array de Accounts filtrando las que no tengan un tipo valido
+    return [new Account(), new Account()]
+  }
+  
+  
+  
+  //NO MODIFICAR
+  window.onload = async ()=>{
+    const API_ENDPOINT = "https://api.npoint.io/e9cb7acc6095dff4004a"
+  
+    const response = await fetchAPI(API_ENDPOINT);
+    console.log("fetchAPI response:", response);
+    const accounts = mapToAccounts(response);
+    console.log("mappToAccounts result", accounts);
+  
+    document.getElementById("main").innerHTML = accounts.reduce((accumulator, currentValue)=>{ return accumulator + currentValue.toHtml() }, "")
+  }
